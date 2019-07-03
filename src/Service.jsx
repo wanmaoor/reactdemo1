db502a923e1d586a154985375b3b4eab7d85a684
wanmaoor <wanmao1284879375@outlook.com>
2019-07-03 10:19:07 +0800

    增加onChange事件的处理函数

--- src/Service.jsx
+++ src/Service.jsx
@@ -1,19 +1,32 @@
-import React, {Component, Fragment} from "react";
-import ReactDOM from "react-dom";
-
+import React, { Component, Fragment } from "react";
 export default class Service extends Component {
-  render(){
-   return (
-    <Fragment>
-      <div>
-        <input type="text"/>
-        <button>add</button>
-      </div>
-      <ul>
-        <li>酒水</li>
-        <li>餐饮</li>
-      </ul>
-    </Fragment>
-   )
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: "",
+      list: []
+    };
+  }
+  render() {
+    return (
+      <Fragment>
+        <div>
+          <input type="text" value={this.state.inputValue} onChange={this.handleChange} placeholder="input service you want"/>
+          <button onClick={this.handleClick}>add</button>
+        </div>
+        <ul>
+          <li>酒水</li>
+          <li>餐饮</li>
+        </ul>
+      </Fragment>
+    );
+  }
+  handleChange = (e)=>{
+    this.setState({
+      inputValue: e.target.value
+    })
+  }
+  handleClick = (e) => {
+
   }
 }
