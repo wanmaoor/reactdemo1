80e72650b5b1f5267146775adf3434e4721c8fa3
wanmaoor <wanmao1284879375@outlook.com>
2019-07-03 16:59:28 +0800

    增加props类型检验

--- src/ServiceItem.jsx
+++ src/ServiceItem.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import PropTypes from 'prop-types'
 export default class ServiceItem extends Component {
 
   render() {
@@ -12,4 +12,11 @@ export default class ServiceItem extends Component {
   handleClick = ()=>{
     this.props.deleteItem(this.props.children)
   }
+}
+
+// propType validation
+ServiceItem.propTypes = {
+  content: PropTypes.string,
+  index: PropTypes.number,
+  deleteItem: PropTypes.func
 }
